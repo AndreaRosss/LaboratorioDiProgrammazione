be0cd1c18e40bf41a48a0fca53db068c5e3bcbb2
AndreaRosss <andrea.rosss@yahoo.com>
2023-12-22 14:58:52 +0100

    Aggiunti wxButton "Aggiungi Attività" e "Sfoglia Registro Attività" a MainFrame.

--- LaboratorioDiProgrammazione/MainFrame.cpp
+++ LaboratorioDiProgrammazione/MainFrame.cpp
@@ -2,4 +2,8 @@
 #include <wx/wx.h>
 #include <string>
 
-MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title){}
+MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title){
+	wxPanel* panel = new wxPanel(this);
+	wxButton* button1 = new wxButton(panel, wxID_ANY, "Aggiungi Attività", wxPoint(350, 250), wxSize(100, 35));
+	wxButton* button2 = new wxButton(panel, wxID_ANY, "Sfoglia Registro Attività", wxPoint(300, 350), wxSize(200, 35));
+}
