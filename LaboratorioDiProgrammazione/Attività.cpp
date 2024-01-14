65bb65929f372fff2da3ac1b238e5891d465280b
AndreaRosss <andrea.rosss@yahoo.com>
2024-01-14 14:16:31 +0100

    Sostituito corpo del costruttore di Attività con lista di inizializzazione.

--- LaboratorioDiProgrammazione/Attività.cpp
+++ LaboratorioDiProgrammazione/Attività.cpp
@@ -1,10 +1,5 @@
 #include "Attività.h"
 using namespace std;
 
-Attività::Attività(wxString Descrizione, wxString Data, wxString OraInizio, wxString OraFine)
-{
-	this->Descrizione = Descrizione;
-	this->Data = Data;
-	this->OraInizio = OraInizio;
-	this->OraFine = OraFine;
-}
+Attività::Attività(wxString descrizione, wxString data, wxString oraInizio, wxString oraFine) : 
+	Descrizione(descrizione), Data(data), OraInizio(oraInizio), OraFine(oraFine){}
