0cad73eb9dcdeec06db26f43c138818ee591dd64
AndreaRosss <andrea.rosss@yahoo.com>
2024-01-16 16:14:30 +0100

    Sovraccaricato operatore !=. Implementata funzione ValiditàOrari e Setter dei vari campi.

--- LaboratorioDiProgrammazione/Attività.h
+++ LaboratorioDiProgrammazione/Attività.h
@@ -5,14 +5,20 @@ using namespace std;
 
 class Attività
 {
+private:
 	wxString Descrizione;
 	wxString Data;
 	wxString OraInizio;
 	wxString OraFine;
 
 public:
-	Attività(const wxString& Descrizione, const wxString& Data, const wxString& OraInizio, const wxString& OraFine);
 
+	Attività(const wxString& Descrizione, const wxString& Data, const wxString& OraInizio, const wxString& OraFine);
+	~Attività() {};
+
+	bool operator!=(const Attività& attività);
+
+	bool ValiditàOrari(const wxString& a, const wxString& b);
 
 	wxString GetData() {
 		return this->Data;
@@ -28,6 +34,22 @@ public:
 
 	wxString GetOraFine() {
 		return this->OraFine;
+	}
+
+	void SetData(const wxString& Data) {
+		this->Data = Data;
+	}
+
+	void SetDescrizione(const wxString& Descrizione) {
+		this->Descrizione = Descrizione;
+	}
+
+	void SetOraInizio(const wxString& OraInizio) {
+		this->OraInizio = OraInizio;
+	}
+
+	void SetOraFine(const wxString& OraFine) {
+		this->OraFine = OraFine;
 	}
 };
 
